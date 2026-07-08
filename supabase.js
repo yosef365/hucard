26c638837a8f7168c128a3e53c19fe8dfe900e0c
yosef365 <35501033+yosef365@users.noreply.github.com>
2026-07-09 01:14:06 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://oypgqdlypqpvbdhvvgaa.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95cGdxZGx5cHFwdmJkaHZ2Z2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NDkwMzksImV4cCI6MjA5OTAyNTAzOX0.XBqbsgprrbxSwsg5w_cy_OIUPmTnKRXUw_ToWrEatb8";
-
-const { createClient } = supabase;
-
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
