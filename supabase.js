7168aa24847657e04a7f3a25eee66ad376803061
yosef365 <35501033+yosef365@users.noreply.github.com>
2026-07-09 00:20:23 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://oypgqdlypqpvbdhvvgaa.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_NEW_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95cGdxZGx5cHFwdmJkaHZ2Z2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NDkwMzksImV4cCI6MjA5OTAyNTAzOX0.XBqbsgprrbxSwsg5w_cy_OIUPmTnKRXUw_ToWrEatb8";
 
 const { createClient } = supabase;
 
